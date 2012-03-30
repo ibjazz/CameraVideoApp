e060815290c39a0c03174b1b2c973b7172cbc4f9
Ignacio Bermeo <ibermeo@me.com>
2012-03-30 11:47:51 -0600

    Initial Commit

---
+++ CameraVideoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraVideoApp
+//
+//  Created by Ignacio Bermeo Juarez on 3/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
